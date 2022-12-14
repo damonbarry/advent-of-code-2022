38ba77525e78820af30c73693adba8b95244d51d
Damon Barry <damonb@microsoft.com>
2022-12-13 19:15:56 -0800

    Solve day 2 part 2

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ fn main() {
     find_elves_carrying_the_most_calories(1);
     find_elves_carrying_the_most_calories(3);
     calculate_rock_paper_scissors_score_for_strategy_guide();
+    calculate_rock_paper_scissors_score_for_corrected_strategy_guide();
 }
 
 fn find_elves_carrying_the_most_calories(num: usize) {
@@ -81,4 +82,37 @@ fn calculate_rock_paper_scissors_score_for_strategy_guide() {
         .sum();
 
     println!("I followed the strategy guide. My score is {}", score);
+}
+
+fn calculate_rock_paper_scissors_score_for_corrected_strategy_guide() {
+    let input = fs::read_to_string("input/day2.txt").unwrap();
+    let scores: Vec<u64> = input.lines().map(|l| {
+        let encoded : Vec<_> = l.split(' ').collect();
+        let decoded: Vec<u64> = encoded.iter().map(|mv| {
+            match *mv {
+                "A" => 1,
+                "B" => 2,
+                "C" => 3,
+                "X" => 0,
+                "Y" => 3,
+                "Z" => 6,
+                _ => panic!("Unknown code '{}'", mv),
+            }
+        }).collect();
+
+        match (decoded[0], decoded[1]) {
+            (1, 0) => 3 + 0, // Rock, lose => scissors
+            (1, 3) => 1 + 3, // Rock, draw => rock
+            (1, 6) => 2 + 6, // Rock, win => paper
+            (2, 0) => 1 + 0, // Paper, lose => rock
+            (2, 3) => 2 + 3, // Paper, draw => paper
+            (2, 6) => 3 + 6, // Paper, win => scissors
+            (3, 0) => 2 + 0, // Scissors, lose => paper
+            (3, 3) => 3 + 3, // Scissors, draw => scissors
+            (3, 6) => 1 + 6, // Scissors, win => rock
+            _ => panic!("Unknown round combo '{:?}'", (decoded[0], decoded[1])),
+        }
+    }).collect();
+
+    println!("I followed the (corrected) strategy guide. My score is {}", scores.iter().sum::<u64>());
 }
